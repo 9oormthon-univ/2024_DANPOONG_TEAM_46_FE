d75900cfaa7967f545637fc5ffb49a9a3ac0a309
Jooeun Jeon <jitchive24@gmail.com>
2024-11-23 00:13:26 +0900

    Create my.js

---
+++ book/src/components/my.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "../css/main.css"; // CSS 파일 가져오기
+
+const MyComponent = () => {
+  return (
+    <div className="v2352_759">
+      <div className="v2352_760">
+        <div className="v2352_761"></div>
+        <div className="v2352_762"></div>
+        <div className="v2352_763"></div>
+      </div>
+      <div className="name"></div>
+      <div className="name"></div>
+      <div className="name"></div>
+      <div className="v2352_768"></div>
+      <div className="v2352_769">
+        <div className="v2352_770">
+          <span className="v2352_771">읽고 있는 도서</span>
+        </div>
+      </div>
+      <div className="name"></div>
+      <div className="v2352_773"></div>
+      <div className="v2352_774">
+        <div className="v2352_775">
+          <span className="v2352_776">사자자리</span>
+          <span className="v2352_777">단풍톤님 반가워요!</span>
+        </div>
+        <div className="v2352_778"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MyComponent;
