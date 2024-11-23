4ab5cbfc78d1ef67ef78fa419d9edcf90fdbee00
JiHye Ryu <durumi922@gmail.com>
2024-11-24 05:47:40 +0900

    💄 Add login

--- book/src/App.js
+++ book/src/App.js
@@ -5,6 +5,8 @@ import ClubPage from './pages/ClubPage' // 북클럽 페이지
 import StarPage from './pages/StarPage' // 별자리 페이지
 import MyLibraryPage from './pages/MyLibrary/MyLibraryPage' // 내 서재 페이지
 import SettingsPage from './pages/SettingsPage/SettingsPage';
+import LoginPage from './pages/LoginPage'
+
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
       <div>
         {/* 라우터 설정 */}
         <Routes>
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<LibraryPage />} /> {/* 메인 페이지 */}
           <Route path="/club" element={<ClubPage />} />
           <Route path="/star" element={<StarPage />} />
---
+++ book/src/pages/LoginPage.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import '../style.css' // 스타일 가져오기
+
+const LoginPage = () => {
+  const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY // .env에 저장된 키
+  const REDIRECT_URI = 'http://localhost:8080/api/v1/oauth/kakao/callback' // 백엔드 리디렉션 URI
+
+  const handleKakaoLogin = () => {
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+    window.location.href = KAKAO_AUTH_URL // 카카오 로그인 페이지로 리디렉션
+  }
+
+  return (
+    <div className="login-page">
+      <img
+        src="/img/logo title.svg"
+        alt="북두칠성 로고"
+        className="login-logo"
+      />
+      <p className="login-subtitle">독서 공유 어플리케이션 북두칠성</p>
+      <img
+        src="/img/kakao_login_medium_wide.png"
+        alt="카카오 로그인 버튼"
+        className="kakao-login-button"
+        onClick={handleKakaoLogin}
+      />
+      <a href="/terms" className="terms-link">
+        이용약관 확인하기
+      </a>
+    </div>
+  )
+}
+
+export default LoginPage
